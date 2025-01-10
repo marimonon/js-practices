ebc960930d30aaf49ecf6400be13d8c88464cbab
marimonon <mappi-@hotmail.co.jp>
2025-01-10 11:33:53 +0900

    FizzとBuzzの実装
    3の倍数の時にFizz、５の倍数になる時にBuzzの実装

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,6 +1,10 @@
 for (let num = 1; num <= 20; num++) {
   if (num % 3 === 0 && num % 5 === 0) {
     console.log("FizzBuzz");
+  } else if (num % 3 === 0) {
+    console.log("Fizz");
+  } else if (num % 5 === 0) {
+    console.log("Buzz");
   } else {
     console.log(`${num}`);
   }
