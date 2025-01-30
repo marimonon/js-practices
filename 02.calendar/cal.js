a8e69e596f9e69c4eac06bffa39a979c31281b20
marimonon <mappi-@hotmail.co.jp>
2025-01-30 11:40:08 +0900

    月のvalidateを実装。
    ターミナルからの入力が1~12の整数かを判断。年でも再利用できる関数に。

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -2,6 +2,21 @@
 
 import minimist from "minimist";
 
+// ターミナルからの入力をminimistで取得
 const inputTime = minimist(process.argv.slice(2));
+const inputMonth = inputTime["m"];
 
-console.log(inputTime);
+// 入力が整数かどうか,最小、最大値の範囲かを判定する
+function validateInteger(value, min, max) {
+  // 整数以外の場合（負の整数は型がbooleanになるのでこちらになる）
+  if (!Number.isInteger(value)) {
+    throw new Error("値は整数で入力してください");
+  }
+  // 範囲外の場合
+  if (value < min || value > max) {
+    throw new Error(`値は${min}~${max}の範囲で入力してください`);
+  }
+  return value;
+}
+
+console.log(validateInteger(inputMonth, 1, 12));
